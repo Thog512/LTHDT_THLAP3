7c5f56ade1ab371b69b0b6980b211235148a16ab
Thog512 <115013206+Thog512@users.noreply.github.com>
2023-05-09 12:40:38 +0700

    Add files via upload

---
+++ lapth3b1/OrderDetail.java
@@ -0,0 +1,35 @@
+
+package lapth3b1;
+
+public class OrderDetail{
+    public int quantity;
+    public Product product;
+
+
+    public OrderDetail(Product product, int quantity) {
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" + "quantity=" + quantity + ", product=" + product + '}';
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }   
+
+}
